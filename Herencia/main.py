57b531b2fe3cb91903834b2e69cf71ddf3ec5dc1
Rodrigo <rodrigocoarodriguez@gmail.com>
2022-03-24 23:38:58 -0400

    Tarea de herencia terminada

--- Herencia/main.py
+++ Herencia/main.py
@@ -7,7 +7,7 @@ from Herencia.bicycle import Bicycle
 if __name__=="__main__":
     trans1: Transport = Transport('Tren', 100)
     trans2: Transport = Land('Tractor', 4923, True)
-    trans3: Transport = Car('Tesla', 4567, True, False)
+    trans3: Transport = Car('Tesla', 4567, False, False)
     trans4: Transport = Bicycle('Plan3000', 65, False, True)
 
     trans_list = [trans1, trans2, trans3, trans4]
